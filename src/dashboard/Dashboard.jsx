2bc4cb623ec549baed7d2c5d8ba55bac9c097f5b
itsrahulsehgal <rahul.sehgal049@gmail.com>
2023-07-04 19:24:23 +0530

    frontend initialised and home page added

---
+++ src/dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import '../App.css';
+function Dashboard() {
+    return (
+        <>
+            <div className="bg-[url(https://file.rendit.io/n/AVkqpFLgG8He4v0daDmN.png)] bg-cover bg-50%_50% bg-blend-normal flex flex-col justify-start gap-5 relative w-full h-[737px] items-start px-32 py-16">
+                <div className="Frame7 w-[1122px] h-[89px] flex-col justify-center items-start gap-[22px]">
+                    <div className=" text-center text-gray-700 text-[36px] font-bold">Welcome to LearnApti website.</div>
+                    <div className=" w-[1122px] text-green-900 text-[24px] font-normal">Start learning the aptitude problems that will help you in your upcoming job exams.</div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Dashboard;
+
+
